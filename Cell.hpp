528ba6fd53278017c5dda92adde2d721ca021152
Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2019-02-21 17:27:21 +0100

    Cell.hpp: make ctor public (instead of having allocator as friend)

--- Cell.hpp
+++ Cell.hpp
@@ -1,17 +1,6 @@
 #ifndef CELL_HPP
 #define CELL_HPP
 
-// TODO: find a better way to detect which class needs to be our friend?
-// friend std::deque<Cell>::allocator_type doesn't work.
-#ifdef __clang__
-  // works for clang 7.0.1 and Apple clang version 9.1.0
-  #define DEQUE_ALLOCATOR std::allocator
-#else
-  // works for GCC 8.2.0
-  #define DEQUE_ALLOCATOR __gnu_cxx::new_allocator
-#endif
-
-#include <memory>
 #include <iostream>
 
 struct Value;
@@ -23,10 +12,10 @@ private:
   Cells& cells;
   Cell* _left = nullptr;
   Cell* _right = nullptr;
-  Cell(long id, Cells& cells);
-  friend class DEQUE_ALLOCATOR<Cell>; // for deque.emplace_*()
   friend std::ostream& operator<<(std::ostream&, const Cell*);
 public:
+  Cell(long id, Cells& cells); // should be private, is public for deque.emplace_*()
+
   Value* value; // current value
   Cell* left();
   Cell* right();
