2d8bba74e4c7b09876b6cc508905bf2dc8f9b21d
Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2019-02-21 18:54:51 +0100

    Action::print(): cosmetics

--- Action.cpp
+++ Action.cpp
@@ -10,5 +10,7 @@ Action::Action(const char* n, State* cs, State* ns, Value* v, Step* s, long& c) 
   name(n), value(v), step(s), count(c), current(cs), next(ns) {}
 
 void Action::print() {
-  std::cout << "Action " << count << ": "  << name << ": set " << current->cell << " from " << current->cell->value << " to " << value << ", move " << step << ", ";
+  std::cout << "Action " << count << ": " << name << ": "
+  << "set " << current->cell << " from " << current->cell->value << " to " << value << ", "
+  << "move " << step << ", ";
 }
