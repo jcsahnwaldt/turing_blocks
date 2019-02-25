d2f3ad4d1dd672c229813c47c017640e9620ea01
Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2019-02-25 17:55:31 +0100

    cpp/untyped: intermediate version

--- cpp/untyped/main.cpp
+++ cpp/untyped/main.cpp
@@ -2,25 +2,59 @@
 #include <stddef.h>
 #include <iostream>
 
-struct Block final {
-  Block* slots[4];
+class block_ptr final {
+public:
+  void** p;
+  friend std::ostream& operator<<(std::ostream&, block_ptr&);
+public:
+  block_ptr(size_t size) :
+    p(new void*[size]) {}
 
-  Block*& slot(size_t index) {
-    return slots[index];
+  ~block_ptr() {
+    delete[] p;
   }
+
+  block_ptr& operator[](size_t index) {
+    return (block_ptr&)p[index];
+  }
+
+  void operator=(const block_ptr& that) {
+    p = that.p;
+  }
+
+  void operator=(void f(block_ptr&)) {
+    p = (void**)f;
+  }
+
+  void operator()(size_t index) {
+    ((void (*)(block_ptr&))p[index])(*this);
+  }
+
 };
 
+std::ostream& operator<<(std::ostream& os, block_ptr& p) {
+  os << &p << "->" << p.p;
+  return os;
+}
+
+void foo(block_ptr& b) {
+  std::cout << "foo(" << b << ")" << std::endl;
+}
+
 int main(void) {
-  Block* b = new Block();
-  Block* c = new Block();
-  b->slot(0) = c;
-  b->slot(0)->slot(1) = b->slot(0);
-  std::cout << b << std::endl;
-  std::cout << c << std::endl;
-  std::cout << b->slot(0) << std::endl;
-  std::cout << b->slots[0] << std::endl;
-  std::cout << c->slots[1] << std::endl;
-  delete c;
-  delete b;
+  block_ptr b(2);
+  block_ptr c(2);
+  b[0] = c;
+  b[0][1] = b[0];
+  b[1] = foo;
+  b(1);
+  std::cout << "&b " << &b << std::endl;
+  std::cout << "b " << b << std::endl;
+  std::cout << "c " << c << std::endl;
+  std::cout << "b[0] " << b[0] << std::endl;
+  std::cout << "b[1] " << b[1] << std::endl;
+  std::cout << "foo " << (void*)foo << std::endl;
+  std::cout << "b.p[0] " << b.p[0] << std::endl;
+  std::cout << "c.p[1] " << c.p[1] << std::endl;
   return 0;
 }
