e52ca487f27fc840ddafbf142f7432e30a47cf14
Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2019-03-04 22:48:08 +0100

    c/typed/cell.c: dynamic polymorphism for left/right

--- c/typed/cell.c
+++ c/typed/cell.c
@@ -7,24 +7,32 @@
 value_t* cell_default_value = NULL;
 unsigned long cell_count = 0;
 
-static cell_t* left(cell_t* c) {
-  if (! c->_left) {
-    cell_t* n = malloc(sizeof(cell_t));
-    init_cell(n, c->id - 1);
-    n->_right = c;
-    c->_left = n;
-  }
+static cell_t* get_left(cell_t* c) {
   return c->_left;
 }
 
-static cell_t* right(cell_t* c) {
-  if (! c->_right) {
-    cell_t* n = malloc(sizeof(cell_t));
-    init_cell(n, c->id + 1);
-    n->_left = c;
-    c->_right = n;
-  }
+static cell_t* get_right(cell_t* c) {
   return c->_right;
+}
+
+static cell_t* init_left(cell_t* c) {
+  cell_t* n = malloc(sizeof(cell_t));
+  init_cell(n, c->id - 1);
+  n->_right = c;
+  n->right = get_right;
+  c->_left = n;
+  c->left = get_left;
+  return n;
+}
+
+static cell_t* init_right(cell_t* c) {
+  cell_t* n = malloc(sizeof(cell_t));
+  init_cell(n, c->id + 1);
+  n->_left = c;
+  n->left = get_left;
+  c->_right = n;
+  c->right = get_right;
+  return n;
 }
 
 void init_cell(cell_t* c, long id) {
@@ -33,8 +41,8 @@ void init_cell(cell_t* c, long id) {
   c->value = cell_default_value;
   c->_left = NULL;
   c->_right = NULL;
-  c->left = left;
-  c->right = right;
+  c->left = init_left;
+  c->right = init_right;
 }
 
 static void free_cells(cell_t* p, bool left) {
